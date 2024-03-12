362707da495ceac1710a91360ff784f1c07ebeba
DewiRhys <dewirhyschappel@gmail.com>
2024-03-12 16:47:41 +0000

    Okk

--- .ipynb_checkpoints/WorldBuildingGenerator-checkpoint.py
+++ .ipynb_checkpoints/WorldBuildingGenerator-checkpoint.py
@@ -145,8 +145,9 @@ if anytrue:
     
 buttonImage = st.button('Generate Image')
 
-REPLICATE_API_TOKEN = "r8_JAeYYpbpAiZkPNcdIUhJKuJnHQ3YiOQ3NW7hz"
+REPLICATE_API_TOKEN = os.getenv('REPLICATE_API_TOKEN')
 REPLICATE_MODEL_ENDPOINTSTABILITY = "stability-ai/sdxl:2b017d9b67edd2ee1401238df49d75da53c523f36e363881e057f5dc3ed3c5b2"
+replicate.api_token = REPLICATE_API_TOKEN
 
 if buttonImage:
     with st.spinner('Generating image...'):
